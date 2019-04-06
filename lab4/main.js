80787a64c4c4315d70b601820535c963246a4a9a
OskarJ <oskarJ@OskarJs-MacBook-Pro.local>
2019-04-06 09:46:39 +0200

    code cleanup, added, dbOperation, caroperation, initdb  iife, classes module

--- lab4/main.js
+++ lab4/main.js
@@ -1,7 +1,11 @@
 var CarsDB = (function() {
+    var state = {
+        over: 'over',
+        below: 'below',
+    };
     var cars = [];
 
-    function findObjBy(type, value) {
+    function _findObjBy(type, value) {
         return cars
             .map(function(car) {
                 return car[type];
@@ -9,40 +13,47 @@ var CarsDB = (function() {
             .indexOf(value);
     }
 
+    function _overFilter(array, value) {
+        return array.filter(function(car) {
+            return car.mocSilnika > value;
+        });
+    }
+
+    function _belowFilter(array, value) {
+        return array.filter(function(car) {
+            return car.mocSilnika < value;
+        });
+    }
+
     function addCar(car) {
-        if (findObjBy('model', car.model) === -1) {
+        if (_findObjBy('model', car.model) === -1) {
             cars.push(car);
             return console.log('added: ' + car.model);
-        } else {
-            return console.log('this model already exist in db');
         }
+        return console.log('this model already exist in db');
     }
 
     function deleteCar(carModel) {
-        var carIndex = findObjBy('model', carModel);
+        var carIndex = _findObjBy('model', carModel);
         cars.splice(carIndex, 1);
         return console.log('removed: ' + carModel);
     }
 
     function updateCar(carModel, value, key) {
-        var carIndex = findObjBy('model', carModel);
+        var carIndex = _findObjBy('model', carModel);
         cars[carIndex][key] = value;
         return console.log('updated: ' + carModel);
     }
 
     function filterByKM(type, value) {
         var filtered;
-        if (type === 'over') {
-            filtered = cars.filter(function(car) {
-                return car.mocSilnika > value;
-            });
-        } else if (type === 'below') {
-            filtered = cars.filter(function(car) {
-                return car.mocSilnika > value;
-            });
+        if (type === state.over) {
+            filtered = _overFilter(cars, value);
+        } else if (type === state.below) {
+            filtered = _belowFilter(cars, value);
         } else {
             console.log('choose type "below" or "over"');
-            return;
+            filtered = null;
         }
 
         return filtered;
@@ -69,85 +80,141 @@ var CarsDB = (function() {
         filterByKM: filterByKM,
     };
 })();
-
-function Vehicle(id, marka, rokProdukcji, mocSilnika, pojemnoscSilnika, kolor) {
-    this.id = id;
-    this.marka = marka;
-    this.rokProdukcji = rokProdukcji;
-    this.mocSilnika = mocSilnika;
-    this.pojemnoscSilnika = pojemnoscSilnika;
-    this.kolor = kolor;
-}
-
-function Car(
-    model,
-    nadwozie,
-    wPrdukcji,
-    id,
-    marka,
-    rokProdukcji,
-    mocSilnika,
-    pojemnoscSilnika,
-    kolor
-) {
-    Vehicle.call(
-        this,
+var classes = (function() {
+    function Vehicle(
         id,
         marka,
         rokProdukcji,
         mocSilnika,
         pojemnoscSilnika,
         kolor
+    ) {
+        this.id = id;
+        this.marka = marka;
+        this.rokProdukcji = rokProdukcji;
+        this.mocSilnika = mocSilnika;
+        this.pojemnoscSilnika = pojemnoscSilnika;
+        this.kolor = kolor;
+    }
+
+    function Car(
+        model,
+        nadwozie,
+        wPrdukcji,
+        id,
+        marka,
+        rokProdukcji,
+        mocSilnika,
+        pojemnoscSilnika,
+        kolor
+    ) {
+        Vehicle.call(
+            this,
+            id,
+            marka,
+            rokProdukcji,
+            mocSilnika,
+            pojemnoscSilnika,
+            kolor
+        );
+        this.model = model;
+        this.nadwozie = nadwozie;
+        this.wPrdukcji = wPrdukcji;
+    }
+
+    Car.prototype = Object.create(Vehicle.prototype);
+    Car.prototype.varructor = Car;
+
+    Car.prototype.start = function() {
+        return console.log('wrrrrr!');
+    };
+
+    Car.prototype.signal = function(direction) {
+        return direction === 'left'
+            ? console.log('tik in left site')
+            : console.log('tik in right site');
+    };
+    return {
+        Car: Car,
+    };
+})();
+
+(function initDbValues() {
+    var Car = classes.Car;
+    var bmwm3 = new Car(
+        'm3',
+        'coupe',
+        true,
+        1,
+        'bmw',
+        2011,
+        420,
+        3999,
+        'czerwony'
     );
-    this.model = model;
-    this.nadwozie = nadwozie;
-    this.wPrdukcji = wPrdukcji;
-}
+    var nissanz3 = new Car(
+        'z3',
+        'coupe',
+        false,
+        2,
+        'nissan',
+        2011,
+        270,
+        3999,
+        'niebieski'
+    );
+    var maluch = new Car(
+        '126p',
+        'hatchback',
+        false,
+        2,
+        'fiat',
+        1972,
+        23,
+        594,
+        'bialy'
+    );
 
-Car.prototype = Object.create(Vehicle.prototype);
-Car.prototype.varructor = Car;
+    CarsDB.addCar(bmwm3);
+    CarsDB.addCar(nissanz3);
+    CarsDB.addCar(maluch);
+})();
 
-Car.prototype.start = function() {
-    return console.log('wrrrrr!');
-};
+(function DbOperation() {
+    var isPlural = function(array) {
+        return array.length > 1 ? true : false;
+    };
 
-Car.prototype.signal = function(direction) {
-    return direction === 'left'
-        ? console.log('tik in left site')
-        : console.log('tik in right site');
-};
+    var findedSuperCar = CarsDB.filterByKM('over', 300);
+    var allCars = CarsDB.getAllCars();
+    var stillProducedCars = CarsDB.filterByIsProduction();
 
-var bmwm3 = new Car('m3', 'coupe', true, 1, 'bmw', 2011, 420, 3999, 'czerwony');
-var nissanz3 = new Car(
-    'z3',
-    'coupe',
-    false,
-    2,
-    'nissan',
-    2011,
-    270,
-    3999,
-    'niebieski'
-);
-var maluch = new Car(
-    '126p',
-    'hatchback',
-    false,
-    2,
-    'fiat',
-    1972,
-    23,
-    594,
-    'bialy'
-);
+    console.log(
+        'All Cars: ',
+        '\n',
+        allCars,
+        '\n \n',
+        'Only super ' +
+            (isPlural(findedSuperCar) ? 'Cars' : 'Car') +
+            ' filtered: ',
+        '\n',
+        findedSuperCar,
+        '\n \n',
+        'Only still produced ' +
+            (isPlural(stillProducedCars) ? 'Cars' : 'Car') +
+            ': ',
+        '\n',
+        stillProducedCars,
+        '\n \n'
+    );
+})();
 
-CarsDB.addCar(bmwm3);
-CarsDB.addCar(nissanz3);
-CarsDB.addCar(maluch);
+(function CarClassActions() {
+    var allCars = CarsDB.getAllCars();
 
-maluch.start();
-
-var findedSuperCar = CarsDB.filterByKM('over', 300);
-var allCars = CarsDB.getAllCars();
-var stillProducedCars = CarsDB.filterByIsProduction();
-console.log(findedSuperCar, allCars, stillProducedCars);
+    console.log('Car start engine: ');
+    allCars[0].start();
+    console.log('\n \n');
+    console.log('Car switch on right signal: ');
+    allCars[0].signal('right');
+})();
