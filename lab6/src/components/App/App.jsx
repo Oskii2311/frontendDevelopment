58d7908d512f633e511c8eb5a5861758a88db61d
Oskii2311 <os.janczak@gmail.com>
2019-04-14 09:19:25 +0200

    init 6 lab, removed consolelog from previous labs

---
+++ lab6/src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../Main/Main";
+
+const App = () => (
+  <div>
+    <Main name={"World"} />
+  </div>
+);
+
+export default App;
---
+++ lab6/src/components/Main/Main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Main = ({ name }) => (
+  <main>
+    <div>Hello {name}</div>
+  </main>
+);
+
+export default Main;
